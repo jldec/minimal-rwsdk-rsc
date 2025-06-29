68cf4d7d3eb446252b845a212de8ea796b256f30
jldec <jurgen@jldec.me>
2025-06-29 23:26:40 +0100

    no border on button

--- src/app/Buttons.tsx
+++ src/app/Buttons.tsx
@@ -13,7 +13,7 @@ export function Buttons() {
     <div className="flex flex-row items-end gap-2 mx-2 my-3">
       <button
         onClick={handleClick}
-        className="border border-gray-400 active:border-gray-200 rounded-md hover:bg-gray-100 hover:text-orange-500 cursor-pointer px-2 py-1"
+        className="bg-gray-200 rounded-md  hover:text-orange-500 active:shadow-sm active:shadow-gray-200 hover:bg-gray-100 cursor-pointer px-2 py-1"
       >
         Render Realtime Clients
       </button>
