e3e4071b137e6643a36949862bc64f6bd2d74551
Jürgen Leschner <jldec@users.noreply.github.com>
2025-07-21 11:30:24 -0400

    extract Sleep, preserve sleep query param, show in Nav, polish (#25)

--- src/app/Page.tsx
+++ src/app/Page.tsx
@@ -4,32 +4,25 @@ import { Url } from './Url'
 import { Headers } from './Headers'
 import { Params } from './Params'
 import { IsDev } from './IsDev'
+import { Sleep } from './Sleep'
 
-async function sleep(s: number) {
-  return new Promise(resolve => setTimeout(resolve, s * 1000))
+const pages: Record<string, string> = {
+  '/': 'Home',
+  '/about': 'About',
+  '/test': 'Test'
 }
 
 export async function Page() {
   const url = new URL(r.request.url)
-  const sleepTime = Number(url.searchParams.get('sleep'))
-  if (sleepTime > 0) {
-    await sleep(sleepTime)
-  }
-  const pathname = new URL(r.request.url).pathname
-  const pages: Record<string, string> = {
-    '/': 'Home',
-    '/about': 'About',
-    '/test': 'Test'
-  }
-  const title = pages[pathname] || pathname
+  const title = pages[url.pathname] || url.pathname
 
   return (
     <Layout>
       <title>{title + ' minimal-rwsdk-rsc'}</title>
       <div className="m-3">
         <h1 className="text-center text-2xl font-bold border-b border-gray-200 mb-2">{title}</h1>
-        {sleepTime > 0 && <p>Sleeping {sleepTime} seconds</p>}
-        <p>This is a server component</p>
+        This is a server component
+        <Sleep />
         <p>{new Date().toISOString()}</p>
         <Url />
         <IsDev />
